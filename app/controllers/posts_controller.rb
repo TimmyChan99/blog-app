e8fa4c2e6693f669e7c3b039547bd1103f74075e
Fatima Ezzahra <felemenoun@gmail.com>
2022-04-27 22:38:24 +0100

    Create posts show page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,7 @@ class PostsController < ApplicationController
 	def index
 		
 	end
+	def show
+		
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get 'posts/index', to: 'posts#index'
+  get 'posts/show', to: 'posts#show'
   # Defines the root path route ("/")
   # root "articles#index"
 end
