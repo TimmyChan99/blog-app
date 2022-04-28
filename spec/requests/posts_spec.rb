d5235f51fe56bb3d6033fe76727212142593cd01
Fatima Ezzahra <felemenoun@gmail.com>
2022-04-28 14:31:00 +0100

    Test GET post#show

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -15,5 +15,21 @@ RSpec.describe "Posts", :type => :request do
 			expect(response.body).to include("list of posts")
 		end
 	end
+
+	describe 'Test Get posts#show' do
+		before(:each) do
+			get '/users/index/posts/show'
+		end
+		it 'Test If the correct template was rendered' do
+			expect(response).to render_template(:show)
+		end
+		it 'Test If response status was correct' do
+			expect(response).to have_http_status(:ok)
+		end
+		it 'Test If the response body includes correct placeholder text' do
+			expect(response.body).to include("List of Posts")
+			expect(response.body).to include("post#show")
+		end
+	end
 	
 end
