916b0d07438d3ad625c45ffce7c978692e9f783a
Fatima Ezzahra <felemenoun@gmail.com>
2022-05-02 23:38:52 +0100

    Create posts_counter method"

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :likes
   has_many :comments
+
+  def posts_counter
+    # User.each do |user| 
+    #   user.update(posts_counter: user.posts.count) if user.id == posts.author_id
+    # end
+    current_user = User.where(["id = :author_id", { author_id: "#{author_id}"}])
+    User.update(posts_counter: current_user.posts.count)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
 	has_many :comments, foreign_key: 'author_id'
 	has_many :likes, foreign_key: 'author_id'
 
-	def recent_tree_posts(id)
-		posts.where(["author_id = :id", { id: "#{id}"}]).order(created_at: :desc).limit(3)
+	def recent_tree_posts
+		Post.where(["author_id = :id", { id: "#{id}"}]).order(created_at: :desc).limit(3)
 	end
 end
