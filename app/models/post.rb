0a67693c8aaabb86c01a702ed8effe57e3899ce7
Fatima Ezzahra <felemenoun@gmail.com>
2022-05-03 00:37:14 +0100

    Make update_posts_counter method private and use Callbacks

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,10 @@ class Post < ApplicationRecord
   has_many :likes
   has_many :comments
 
+  after_save :update_posts_counter
+
+  private
+
   def update_posts_counter
     @user = User.find(author_id)
     @user.update(posts_counter: @user.posts.count)
