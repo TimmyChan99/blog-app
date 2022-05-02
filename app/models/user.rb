01aa00cf371fc3860399bcef4cd2362ccc495309
Fatima Ezzahra <felemenoun@gmail.com>
2022-05-02 21:35:35 +0100

    Create recent_tree_post method in model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
 	has_many :posts, foreign_key: 'author_id'
 	has_many :comments, foreign_key: 'author_id'
 	has_many :likes, foreign_key: 'author_id'
+
+	def recent_tree_posts(id)
+		posts.where(["author_id = :id", { id: "#{id}"}]).order(created_at: :desc).limit(3)
+	end
 end
