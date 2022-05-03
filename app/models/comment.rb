73c26be6f1b90c4cb32b534eb8d88152ff0aa6a0
Fatima Ezzahra <felemenoun@gmail.com>
2022-05-03 01:12:25 +0100

    Create update_comments_couter method

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,13 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
+
+  after_save :update_comments_counter
+
+  private
+
+  def update_comments_counter
+    @post = Post.find(post_id)
+    @post.update(comments_counter: @post.comments.count)
+  end
 end
