7f55bfabeebd5fc66c040a306aef0f8815a02c1b
Fatima Ezzahra <felemenoun@gmail.com>
2022-05-03 01:20:48 +0100

    Create update_likes_counter method

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,13 @@
 class Like < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
+
+  after_save :update_likes_counter
+
+private
+
+  def update_likes_counter
+    @post = Post.find(post_id)
+    @post.update(likes_counter: @post.likes.count)
+  end
 end
