49bcc7b415e98a97ac47833ca4ea3e9cb5fcf731
Fatima Ezzahra <felemenoun@gmail.com>
2022-05-05 02:28:42 +0100

    Style posts#index view and display data

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,9 @@
 class PostsController < ApplicationController
-  def index; end
+  def index
+    @users = User.all
+    @user = User.find(params[:user_id])
+    @posts = Post.where(['author_id = :id', { id: params[:user_id].to_s }])
+  end
 
   def show; end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,6 @@ class UsersController < ApplicationController
   end
 
   def show
-    console
   @user = User.find(params[:id])
   @posts = @user.recent_tree_posts
   end
