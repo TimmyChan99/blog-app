381f41dda7ee4aea55818afef91eca990ec8157a
Fatima Ezzahra <felemenoun@gmail.com>
2022-05-06 20:31:08 +0100

    Add test for Comment model

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  subject{ Comment.new(author_id: 2, post_id: 5, text: 'comment') }
+    
+
+  it 'is valid with valid attributes' do
+    expect(subject).to_not be_valid
+  end
+
+  it 'is valid with valid post_id' do
+    subject.post_id = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'is valid with valid author_id' do
+    subject.author_id = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'is valid with valid attributes types' do
+    expect(subject.author_id).to be_a(Numeric)
+    expect(subject.post_id).to be_a(Numeric)
+    expect(subject.text).to be_a(String)
+  end
 end
