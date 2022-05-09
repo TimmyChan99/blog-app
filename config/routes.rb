be75325fae4c864b4dcd79e03e0fa72e14f39747
Fatima Ezzahra <felemenoun@gmail.com>
2022-05-09 11:28:25 +0100

    Create costum routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
   
-   root "devise/sessions#new"
+   root "users#index"
 
-   devise_for :users
+   devise_for :users,
+    path: '', # optional namespace or empty string for no space
+    path_names: {
+      sign_in: 'login',
+      sign_out: 'logout',
+      password: 'secret',
+      confirmation: 'verification',
+      registration: 'register',
+      sign_up: 'signup'
+    }
 
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
