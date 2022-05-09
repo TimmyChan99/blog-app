792a0c5a56c4aa90b3126fe55fa03eb0035f97e7
Fatima Ezzahra <felemenoun@gmail.com>
2022-05-09 11:24:31 +0100

    Setup devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   
-   root "users#index"
+   root "devise/sessions#new"
+
+   devise_for :users
+
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
       resources :comments, only: [:new, :create]
