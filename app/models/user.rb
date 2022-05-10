c1f8e3772f718679126f0847ef0d148fa9f7d24e
Fatima Ezzahra <felemenoun@gmail.com>
2022-05-10 22:35:00 +0100

    Add authorization to delete a post for admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,6 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-   Roles = [ :admin , :default ]
 
   def recent_tree_posts
     Post.where(['author_id = :id', { id: id.to_s }]).order(created_at: :desc).limit(3)
