b4f791c11d5b1a5f0ece1cbe5849892121a2cbf4
Fatima Ezzahra <felemenoun@gmail.com>
2022-05-10 20:01:55 +0100

    Add ability to delete a post

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
     }
 
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new, :create] do
+    resources :posts, only: [:index, :show, :new, :create, :destroy] do
       resources :comments, only: [:new, :create]
       resources :likes, only: [:new, :create]
     end
