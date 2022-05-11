312ad8310e2b27c5483be45faadb15a33bd11ee7
joskalenda <joskalenda3@gmail.com>
2022-05-12 00:30:59 +0200

    App testing for user#show

--- spec/feature/user_show_spec.rb
+++ spec/feature/user_show_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.describe 'Blog app', type: :feature do
   describe ' user index show page' do
     before(:each) do
-      @user1 = User.create!(name: 'User1', photo: '', bio: '', posts_counter: 3, created_at: Time.now, role: 'admin', confirmed_at: Time.now, email: "user1@gmail.com", password: '000000', id: 1)
-      @post1 =  Post.create!(text: 'I like this moroco', title: 'title post',comments_counter: 3, likes_counter: 8, created_at: Time.now, author_id: @user1.id)
-      visit users_path(@user1)
+      @user1 = User.create!(name: 'User1', photo: '', bio: 'this is the bio', posts_counter: 2, created_at: Time.now, role: 'admin', confirmed_at: Time.now, email: "user1@gmail.com", password: '000000', id: 1)
+      @post = @user1.posts.create!(title: "Sample", text: 'lolololo')
+      visit user_posts_path(@user1)
     end
 
     it 'Should see the user\'s profile picture' do
@@ -13,5 +13,33 @@ RSpec.describe 'Blog app', type: :feature do
       users_profile = page.all('div h2')
       expect(images.size).to eql(users_profile.size)
     end
+
+    it "Should see the username of the user" do
+      expect(page).to have_content(@user1.name)
+    end
+
+    it "Should see each user's post number" do
+      page.has_selector?('p', text: 'Number of posts: 2')
+    end
+
+    it "Should see the user's bio" do
+      page.has_selector?('article', text: 'this is the bio')
+    end
+
+    it 'Should  the user\'s first three posts' do
+      posts = page.all('li p')
+      expect(posts.length).to be < 4
+    end
+
+    it 'Should redirects to that post\'s show page.' do
+      click_link 'Sample'
+      expect(page).to have_current_path(user_post_path(@user1, @post))
+    end
+
+    # it 'Should redirects to that post\'s show page.' do
+    #   click_button "See all posts"
+    #   expect(page).to have_current_path(user_posts_path(@user1))
+    # end
+
   end
 end
