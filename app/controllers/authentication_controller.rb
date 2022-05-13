abe167f9fa7c7b911f8fef970719cd9e9c14afe4
joskalenda <joskalenda3@gmail.com>
2022-05-13 16:56:47 +0200

    Add token to Auth_controller

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -8,7 +8,7 @@ class AuthenticationController < ApplicationController
     @user = User.find_by_email(params[:email])
     if @user&.valid_password?(params[:password])
       token = jwt_encode(user_id: @user.id)
-      render json: { token: }, status: :ok
+      render json: { token: token }, status: :ok
     else
       render json: { error: 'unauthorized' }, status: :unauthorized
     end
