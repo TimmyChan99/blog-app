7a59b31d43c69e2e8f9f396098217887fa656674
joskalenda <joskalenda3@gmail.com>
2022-05-13 16:39:41 +0200

    Add index comments controller

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::CommentsController < ApplicationController
   before_action :authenticate_request, only: [:create]
 
- 
+  def index
+    @comments = Comment.where(['post_id = :id', { id: params[:post_id].to_s }])
+    render json: JSON.pretty_generate(@comments.as_json)
+  end
+
 end
