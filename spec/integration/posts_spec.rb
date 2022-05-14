e291c3e21a12fbb656b3dbc28498a8a9f21d765f
TimmyChan99 <felemenoun@gmail.com>
2022-05-15 00:05:34 +0100

    Add create user api test

--- spec/integration/posts_spec.rb
+++ spec/integration/posts_spec.rb
@@ -1,25 +1,18 @@
 require 'swagger_helper'
 
-describe 'Posts API' do
+describe 'Register and Login API' do
 
-  path '/api/v1/users/4/posts' do
+  path '/api/v1/users/' do
 
-    post 'Access posts for a users' do
-      tags 'Posts'
+    post 'Create a user' do
+      tags 'user'
       consumes 'application/json'
-			parameter name: :Authorization, in: :header, type: :string
-      parameter name: :posts, in: :body, schema: {
-        type: :object,
-        properties: {
-          user_id: { type: :integer },
-          post_id: { type: :integer }
-        },
-        required: %w[user_id post_id]
-      }
 
-      response '201', 'pet created' do
-        let(:posts) { { user_id: 1, posts: 4, status: 'available' } }
-        run_test!
+      response '422', 'pet created' do
+				run_test! do |response|
+					data = JSON.parse(response.body)
+					expect(data['message']).to eq('Something is wrong')
+				end
       end
 
       
